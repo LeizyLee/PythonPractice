067bd891d0c5524c4a7c4de0fc2162fa0270973d
LeizyLee <36654052+LeizyLee@users.noreply.github.com>
2018-10-15 17:48:55 +0900

    Update dirget.c

--- dirget.c
+++ dirget.c
@@ -12,19 +12,19 @@
 여기서 맨 아래의 3개 time_t 를 사용하기 위해 위에 time.h 를 포함해준다.
 
 struct stat {
-    dev_t     st_dev;     /* ID of device containing file */
-    ino_t     st_ino;     /* inode number */
-    mode_t    st_mode;    /* protection */
-    nlink_t   st_nlink;   /* number of hard links */
-    uid_t     st_uid;     /* user ID of owner */
-    gid_t     st_gid;     /* group ID of owner */
-    dev_t     st_rdev;    /* device ID (if special file) */
-    off_t     st_size;    /* total size, in bytes */
-    blksize_t st_blksize; /* blocksize for file system I/O */
-    blkcnt_t  st_blocks;  /* number of 512B blocks allocated */
-    time_t    st_atime;   /* time of last access */
-    time_t    st_mtime;   /* time of last modification */
-    time_t    st_ctime;   /* time of last status change */
+    dev_t     st_dev;      ID of device containing file 
+    ino_t     st_ino;      inode number 
+    mode_t    st_mode;     protection 
+    nlink_t   st_nlink;    number of hard links 
+    uid_t     st_uid;      user ID of owner 
+    gid_t     st_gid;      group ID of owner 
+    dev_t     st_rdev;     device ID (if special file) 
+    off_t     st_size;     total size, in bytes 
+    blksize_t st_blksize;  blocksize for file system I/O 
+    blkcnt_t  st_blocks;   number of 512B blocks allocated 
+    time_t    st_atime;    time of last access 
+    time_t    st_mtime;    time of last modification 
+    time_t    st_ctime;    time of last status change 
 };
 
 참고(이후 참고는 최하단에 링크만 기입) : https://linux.die.net/man/2/stat
